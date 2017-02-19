a9c4d0c1b8b3c03c5a59c0ba860fc68bee889d90
Dylan Avery <fossilfuelmusic@gmail.com>
2017-02-18 18:16:23 -0700

    css stuff

--- test/src/App.js
+++ test/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './record1.png';
 import './App.css';
 
 class App extends Component {
@@ -8,11 +8,9 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Rap Genius Genius</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">Hello World</p>
       </div>
     );
   }
