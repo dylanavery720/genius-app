5da6096d23af7d386a43f5bb0243b123d5c142f8
Dylan Avery <fossilfuelmusic@gmail.com>
2017-02-21 20:02:30 -0700

    finally get redux working, debating on whether or not to make this a front end app

--- test/src/Components/SongCards/SongCards.js
+++ test/src/Components/SongCards/SongCards.js
@@ -2,38 +2,46 @@ import React, { Component } from 'react';
 // export class SongCards extends Component = (props) => {
 export default class SongCards extends Component {
 
-  loadSongs() {
-    if(this.props.popularSongs.length < 1) {
-    return this.props.popularSongs.map((song, i) => {
-       return (
-      <li className='card' key={i}>
-       <img alt='alt' src={song.result.song_art_image_thumbnail_url}/>
-       <a href="`https://genius.com/${song.result.path}`">{song.result.full_title}</a>
-      </li>
-       )
-     })
-   } else {
-     return this.props.searchedSongs.map((song, i) => {
-       let songPath = `https://genius.com/${song.result.path}`
-        return (
-          <div>
-              <h1>HEY RENDER</h1>
-        <li className='card' key={i}>
-          <img alt='alt' src={song.result.song_art_image_thumbnail_url}/>
-          <a href={songPath} target="_blank">{song.result.full_title}</a>
-        </li>
-        </div>
-        )
-      })
-   }
-  }
+  // loadSongs() {
+  //   if(this.props.popularSongs.length < 1) {
+  //   return this.props.popularSongs.map((song, i) => {
+  //      return (
+  //     <li className='card' key={i}>
+  //      <img alt='alt' src={song.result.song_art_image_thumbnail_url}/>
+  //      <a href="`https://genius.com/${song.result.path}`">{song.result.full_title}</a>
+  //     </li>
+  //      )
+  //    })
+  //  } else {
+  //    return this.props.searchedSongs.map((song, i) => {
+  //      let songPath = `https://genius.com/${song.result.path}`
+  //       return (
+  //         <div>
+  //             <h1>HEY RENDER</h1>
+  //       <li className='card' key={i}>
+  //         <img alt='alt' src={song.result.song_art_image_thumbnail_url}/>
+  //         <a href={songPath} target="_blank">{song.result.full_title}</a>
+  //       </li>
+  //       </div>
+  //       )
+  //     })
+  //  }
+  // }
 
 
   render() {
   return (
       <div className='body-mask'>
       <ul>
-      {this.loadSongs()}
+      {this.props.searchedSongs.map((song, i) => {
+        let songPath = `https://genius.com/${song.result.path}`
+         return (
+         <li className='card' key={i}>
+           <img alt='alt' src={song.result.song_art_image_thumbnail_url}/>
+           <a href={songPath} target="_blank">{song.result.full_title}</a>
+         </li>
+         )
+       })}
       </ul>
       </div>
   )
