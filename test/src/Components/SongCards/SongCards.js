5795c38e3d4980897c265cf26081a0da2f42a260
Dylan Avery <fossilfuelmusic@gmail.com>
2017-02-22 18:04:37 -0700

    refactor and revamp

--- test/src/Components/SongCards/SongCards.js
+++ test/src/Components/SongCards/SongCards.js
@@ -3,15 +3,14 @@ import React, { Component } from 'react';
 export default class SongCards extends Component {
 
   loadSongs() {
-
+        let shortArray = this.props.searchedSongs.splice(0,6)
         return (
           <div>
-          {this.props.searchedSongs.map((songs, i) => {
+          {shortArray.map((songs, i) => {
             let str = songs.uri
             let newStr = str.slice(0,6) + str.slice(7, str.length)
           return    <iframe src={`//player.vimeo.com${newStr}`} width="480" height="270" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
           })}
-
           </div>
         )
    }
@@ -21,17 +20,6 @@ export default class SongCards extends Component {
     const {searchedSongs} = this.props
   return (
       <div className='body-mask'>
-      {/* <ul>
-      {this.props.searchedSongs.map((song, i) => {
-        let songPath = `https://genius.com/${song.result.path}`
-         return (
-         <li className='card' key={i}>
-           <img alt='alt' src={song.result.header_image_thumbnail_url}/>
-           <a href={songPath} target="_blank">{song.result.full_title}</a>
-         </li>
-         )
-       })}
-      </ul> */}
       {this.loadSongs()}
       </div>
   )
