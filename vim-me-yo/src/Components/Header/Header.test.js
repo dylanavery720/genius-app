331110917321f44f6f13c0b21562e290e89514aa
Dylan Avery <fossilfuelmusic@gmail.com>
2017-03-01 12:48:38 -0700

    halfway done with header tests

--- vim-me-yo/src/Components/Header/Header.test.js
+++ vim-me-yo/src/Components/Header/Header.test.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import sinon from 'sinon';
+import { shallow, mount } from 'enzyme';
 import Header from './Header';
 
 it('renders without crashing', () => {
-  const handleClick = sinon.spy()
   const div = document.createElement('div');
-  ReactDOM.render(<Header handleClick={handleClick} />, div);
+  ReactDOM.render(<Header />, div);
+});
+
+it('accepts a dynamic className', () => {
+  const className = 'header-class'
+  const newHeader = mount(<Header classes={className} />)
+  expect(newHeader.find('.header-class').length).toEqual(1)
+});
+
+it('should return a login button if no access token present', () => {
+  const wrapper = mount(<Header />)
+  const login = <a href="http://localhost:9000/test">Login</a>
+    expect(wrapper.contains(login)).toEqual(true)
+});
+
+it('should make an API call when search is being input', () => {
+  const updateSearch = jest.fn()
+  const wrapper = mount(<Header updateSearch={updateSearch} />)
+  const input = wrapper.find('input')
+  input.simulate('change')
+  expect(updateSearch.mock.calls.length).toBe(1)
+});
+
+it.skip('should return a logout button and welcome message if access token present', () => {
+  const tokenProps = {
+    user: {
+      name: 'D Diddy Bop',
+    },
+    token: '1248192470',
+  }
+  const wrapper = mount(<Header token={tokenProps}/>)
+  const logOut = jest.fn()
+  const welcome = <h3>WELCOME, {tokenProps.user.name}</h3>
+  const logout = <a onClick={logOut}>Log Out</a>
+    expect(wrapper.contains(logout)).toEqual(true)
+    expect(wrapper.contains(userName)).toEqual(true)
 });
