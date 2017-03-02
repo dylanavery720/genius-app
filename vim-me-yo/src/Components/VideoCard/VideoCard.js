39002316b788c4b6c54b611f427e035de57b2afc
Dylan Avery <fossilfuelmusic@gmail.com>
2017-03-01 21:31:00 -0700

    working on unique

--- vim-me-yo/src/Components/VideoCard/VideoCard.js
+++ vim-me-yo/src/Components/VideoCard/VideoCard.js
@@ -7,9 +7,12 @@ const VideoCard = (props) => {
     <li className={props.classes} key={props.keys}>
       {shortTags.map(tag => <h3 className="mdl-card__title-text">&nbsp; #{tag.canonical}</h3>)}
       <iframe src={props.src} width="480" height="270" />
-      <button type="checkbox" id="smiley" className="mdl-button mdl-js-button mdl-button--icon">
-        <i className="material-icons" id={props.id} onClick={props.favorite}>mood</i>
-      </button>
+      {!props.toggleC && <button type="checkbox" id="smiley" className="mdl-button mdl-js-button mdl-button--icon">
+          <i className="material-icons" id={props.id} onClick={props.favorite}>mood</i>
+        </button>}}
+      {props.toggleC && <button type="checkbox" id="smiley" className="mdl-button mdl-js-button mdl-button--icon mdl-button--colored">
+            <i className="material-icons" id={props.id} onClick={props.favorite}>mood</i>
+          </button>}
     </li>
   )
 }
