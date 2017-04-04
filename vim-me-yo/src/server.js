c5d6dc595c50c0e064a2e456c45df560fe5e98bb
Dylan Avery <fossilfuelmusic@gmail.com>
2017-04-04 09:51:12 -0600

    Ready to add headers to fetch

--- vim-me-yo/src/server.js
+++ vim-me-yo/src/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const request = require('request');
+const axios = require('axios');
 const bodyParser = require('body-parser');
 const path = require('path');
 
@@ -9,9 +9,9 @@ let apiKey;
 
 const oauth = {
   clientId: '50c84f82fb4ac0e63c1b94c968aea70558738f33',
-  redirectUri: 'http://localhost:9000/callback',
+  redirectUri: 'http://localhost:9000/',
   scope: 'vote create_annotation manage_annotation me',
-  clientSecret: process.env.VIMEO_CLIENT_SECRET,
+  clientSecret: process.env.ARTSY_CLIENT_SECRET,
 }
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,26 +19,31 @@ app.use(bodyParser.json())
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 
-app.get('/test', (request, response, next) => {
-  const authUrl = `https://api.vimeo.com/oauth/authorize?client_id=${oauth.clientId}&response_type=code&redirect_uri=${oauth.redirectUri}&state=90059`
-  response.redirect(authUrl)
-})
+// app.get('/test', (request, response, next) => {
+//   const authUrl = `https://api.vimeo.com/oauth/authorize?client_id=${oauth.clientId}&response_type=code&redirect_uri=${oauth.redirectUri}&state=90059`
+//   response.redirect(authUrl)
+// })
 
 
-app.get('/callback', (req, res, next) => {
+app.get('/test', (req, res, next) => {
   const options = {
-    url: 'https://api.vimeo.com/oauth/access_token',
+    url: 'https://api.artsy.net/api/tokens/xapp_token',
     form: {
-      code: req.query.code,
-      client_secret: process.env.VIMEO_CLIENT_SECRET,
-      grant_type: 'authorization_code',
-      client_id: oauth.clientId,
+      client_secret: process.env.ARTSY_CLIENT_SECRET,
+      client_id: '41fe04bd2b44a862c2a9',
       redirect_uri: oauth.redirectUri,
-      response_type: 'code',
     },
   }
-  request.post(options, (error, response) => {
-    apiKey = JSON.parse(response.body)
+  axios.post('https://api.artsy.net/api/tokens/xapp_token', {
+    client_secret: process.env.ARTSY_CLIENT_SECRET,
+    client_id: '41fe04bd2b44a862c2a9',
+  })
+  .then((response) => {
+    console.log(response.data.token)
+    apiKey = response.data.token
+  })
+  .catch((error) => {
+    console.log(error)
   })
   res.redirect('/')
 })
@@ -55,4 +60,5 @@ app.get('/*', (req, res) => {
 })
 
 app.listen(9000, () => {
+  console.log("listening at 9000")
 })
